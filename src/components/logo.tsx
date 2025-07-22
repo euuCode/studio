ceb56391382f03a560ab2fc02cc61a519b34b76d
Informática Sitraemg <informatica@sitraemg.org.br>
2025-07-22 17:42:17 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Logo className="t

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -8,15 +8,15 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      strokeWidth="2"
+      strokeWidth="1.5"
       strokeLinecap="round"
       strokeLinejoin="round"
       className={cn('h-8 w-8', props.className)}
       {...props}
     >
-      <circle cx="12" cy="12" r="10" />
-      <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
-      <path d="M12 17h.01" />
+      <path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z" />
+      <circle cx="12" cy="12" r="3" />
+      <path d="M12 17a5 5 0 0 0 0-10" />
     </svg>
   );
 }
